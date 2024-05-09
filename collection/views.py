47eceedf9099fea45582fd521399f54e0179e24a
Muhammad Oka <oka@muhammadoka.dev>
2024-05-10 00:06:08 +0700

    feat: add collection views skeleton

--- collection/views.py
+++ collection/views.py
@@ -1,3 +1,15 @@
+from collection.models import Card
 from django.shortcuts import render
 
 # Create your views here.
+def view_collection(request):
+    collection = Card.objects.filter(user=request.user)
+    context = {'collection': collection}
+
+    return render(request, "", context)
+
+def add_cards_to_collection(request):
+    pass
+    
+def remove_cards_from_collection(request):
+    pass
