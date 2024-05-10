3717aa4d074e4a80b215a2dd04af849c9165411e
Muhammad Oka <oka@muhammadoka.dev>
2024-05-10 20:30:31 +0700

    feat: finish collection backend

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,6 @@
-from collection.models import Card
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from collection.models import Card, Collection
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,10 +8,24 @@ def view_collection(request):
     collection = Card.objects.filter(user=request.user)
     context = {'collection': collection}
 
-    return render(request, "", context)
+    return render(request, "collections.html", context)
 
-def add_cards_to_collection(request):
-    pass
+def add_cards_to_collection(request, cards_name):
+    if request.method == 'POST':
+        for card_name in cards_name:
+            Collection.objects.create(
+                user = request.user,
+                card = Card.objects.get(name=card_name)
+            )
+
+        return HttpResponseRedirect(reverse('collection:view_collection'))
+
+    context = {'owned_cards': Collection.objects.filter(user=request.user)}
+
+    return render(request, "add_cards.html", context)
     
-def remove_cards_from_collection(request):
-    pass
+def remove_cards_from_collection(request, card_name):
+    if request.method == 'POST':
+        Collection.objects.get(name=card_name).delete()
+
+        return HttpResponseRedirect(reverse('collection:view_collection'))
