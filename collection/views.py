0cffff3ac022f585482ea82a74ed1c247aaac10c
Muhammad Oka <oka@muhammadoka.dev>
2024-05-23 20:36:01 +0700

    feat: basic collection frontend functionality

--- collection/views.py
+++ collection/views.py
@@ -5,27 +5,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def view_collection(request):
-    collection = Card.objects.filter(user=request.user)
-    context = {'collection': collection}
+    collection = Collection.objects.filter(user=request.user)
+    context = {'collection': [c.card for c in collection]}
 
     return render(request, "collections.html", context)
 
-def add_cards_to_collection(request, cards_name):
+def add_cards_to_collection(request):
     if request.method == 'POST':
-        for card_name in cards_name:
-            Collection.objects.create(
-                user = request.user,
-                card = Card.objects.get(name=card_name)
-            )
+        cards_id = request.POST.getlist('cards')
+        for card_id in cards_id:
+            card = Card.objects.get(id=int(card_id))
+            Collection.objects.get_or_create(user=request.user, card=card)
 
-        return HttpResponseRedirect(reverse('collection:view_collection'))
+        return HttpResponseRedirect("/collection/")
 
-    context = {'owned_cards': Collection.objects.filter(user=request.user)}
+    context = {'owned_cards': [c.card for c in Collection.objects.filter(user=request.user.id)], 'all_cards': Card.objects.all()}
 
     return render(request, "add_cards.html", context)
     
-def remove_cards_from_collection(request, card_name):
+def remove_cards_from_collection(request, card_id):
     if request.method == 'POST':
-        Collection.objects.get(name=card_name).delete()
+        try:
+            Collection.objects.get(card=Card.objects.get(id=int(card_id)), user=request.user.id).delete()
+        except:
+            pass
 
         return HttpResponseRedirect(reverse('collection:view_collection'))
