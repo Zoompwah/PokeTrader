5e63dc1a2bbef3db0b1da883cd677e5534b42c38
rauloaul <rafif.firmansyah@gmail.com>
2024-05-24 19:37:43 +0700

    REFACTOR: Login Page redirect

--- decks/views.py
+++ decks/views.py
@@ -7,20 +7,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def view_decks(request):
     deck_list = Deck.objects.all().order_by('-deck_rating')
-    paginator = Paginator(deck_list, 4)  # Menampilkan 4 dek per halaman
+    paginator = Paginator(deck_list, 4)
     page = request.GET.get('page')
     try:
         decks = paginator.page(page)
     except PageNotAnInteger:
-        # Jika 'page' bukan integer, tampilkan halaman pertama
         decks = paginator.page(1)
     except EmptyPage:
-        # Jika 'page' lebih besar dari jumlah halaman yang tersedia, tampilkan halaman terakhir
         decks = paginator.page(paginator.num_pages)
     
     for deck in decks:
         deck.deck_rating = "{:.1f}".format(deck.deck_rating)
-
     return render(request, 'decks.html', {'decks': decks})
 
 
@@ -34,13 +31,12 @@ def view_deck_detail(request, deck_id):
 
     print(is_trader)
 
-    # Format deck rating to display only one digit after the decimal point
     deck_rating_formatted = "{:.1f}".format(deck.deck_rating)
     cards = [c.card for c in deck_cards]
 
     context = {
         'deck': deck,
-        'deck_rating_formatted': deck_rating_formatted,  # Add formatted rating to the context
+        'deck_rating_formatted': deck_rating_formatted,
         'cards': cards,
         'is_trader': is_trader,
     }
@@ -49,45 +45,39 @@ def view_deck_detail(request, deck_id):
 def add_deck_rating(request, deck_id):
     if request.method == 'POST':
         deck = get_object_or_404(Deck, pk=deck_id)
-        new_rating = float(request.POST.get('rating'))
+        new_rating = request.POST.get('rating')
 
         if request.user.is_anonymous:
             messages.error(request, "You must be logged in to rate a deck.")
             return redirect('view_deck_details', deck_id=deck_id)
-        
-        # Check if the user has already rated the deck
+    
         if UserRating.objects.filter(user=request.user, deck=deck).exists():
             messages.error(request, "You have already rated this deck.")
             return redirect('view_deck_details', deck_id=deck_id)
         
-        # Validate rating (e.g., between 0 and 5)
+        if new_rating == "":
+            new_rating = 0
+            messages.error(request, "Invalid rating. Please enter a rating between 0 and 5.")
+            return redirect('view_deck_details', deck_id=deck_id)
+        
+        new_rating = float(new_rating)
         if 0 <= new_rating <= 5:
-            # Get all ratings for the deck
+            
             ratings = [deck.deck_rating for deck in Deck.objects.filter(id=deck_id)]
             
             if len(ratings) == 1 and ratings[0] == 0:
                 ratings = []
-                
-            # Add the new rating to the list
             ratings.append(new_rating)
-            
-            # Calculate the average rating
             average_rating = sum(ratings) / len(ratings)
-            
-            # Update the deck's rating
             deck.deck_rating = average_rating
             deck.save()
             
-            # Save the user's rating to prevent them from rating again
             UserRating.objects.create(user=request.user, deck=deck, rating=new_rating)
-
             messages.success(request, 'Rate successful!')
-            
             return redirect('view_deck_details', deck_id=deck_id)
         else:
             messages.error(request, "Invalid rating. Please enter a rating between 0 and 5.")
     
-    # If not a POST request or invalid rating, or user not logged in, redirect to deck detail
     return redirect('view_deck_details', deck_id=deck_id)
 
 @login_required
@@ -100,7 +90,6 @@ def create_deck(request):
         print("deck_card")
         print(deck_card)
 
-        # Assuming the trader is the currently logged-in user
         trader = request.user
 
         deck = Deck(
@@ -124,19 +113,15 @@ def create_deck(request):
 def delete_deck(request, deck_id):
     deck = get_object_or_404(Deck, pk=deck_id)
     
-    # Check if the deck belongs to the current user
     if deck.trader != request.user:
         messages.error(request, "You are not authorized to delete this deck.")
         return redirect('view_deck_details', deck_id=deck.pk)
 
     if request.method == 'POST':
-        # Delete associated DeckCard objects
         deck.deckcard_set.all().delete()
-        
-        # Delete the deck itself
         deck.delete()
 
         messages.success(request, "Deck deleted successfully.")
-        return redirect('/decks')  # Redirect to the home page or any other appropriate page after deletion
+        return redirect('/decks')
 
     return render(request, 'deck_delete.html', {'deck': deck})
